81fc3ccf1fccf667f43821365564cc8a031d77a9
Hancer <hancer.mercede@arroyoconsulting.net>
2025-10-11 21:50:58 -0400

    Update the portfolio with a modern style.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Navigation } from "./components/Navigation";
 import { About } from "./components/About";
 import { Header } from "./components/Header";
 import { TechStackList } from "./components/TechStackList";
@@ -8,13 +9,30 @@ import "animate.css";
 import "./index.css";
 function App() {
   return (
-    <div className="container  animate__animated animate__fadeIn">
-      <Header />
-      <About />
-      <TechStackList />
-      <WorkExpirience />
-      <ProjectList />
-    </div>
+    <>
+      <Navigation />
+      <div className="container animate__animated animate__fadeIn">
+        <section id="home">
+          <Header />
+        </section>
+        
+        <section id="about">
+          <About />
+        </section>
+        
+        <section id="tech-stack">
+          <TechStackList />
+        </section>
+        
+        <section id="experience">
+          <WorkExpirience />
+        </section>
+        
+        <section id="projects">
+          <ProjectList />
+        </section>
+      </div>
+    </>
   );
 }
 
